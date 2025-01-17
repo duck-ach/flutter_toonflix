976473f2fc4c6556d3cce88607cfbcfdbf084f62
duck-ach <djagmlfk@naver.com>
2025-01-17 20:02:52 +0900

    feat: futurebuilder'

--- webtoon/lib/screens/home_screen.dart
+++ webtoon/lib/screens/home_screen.dart
@@ -2,33 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 import 'package:webtoon/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       appBar: AppBar(
         elevation: 0, // 음영
@@ -39,6 +19,18 @@ class _HomeScreenState extends State<HomeScreen> {
           "Today's Toons",
           style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
         ),
+      ),
+      // builder  == 화면을 그려주는 메소드
+      // context  == 내용
+      // snapshot == builder의 상태를 알 수 있음
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data !");
+          }
+          return const Text("Loading...");
+        },
       ),
     );
   }
